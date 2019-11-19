ade52047c088565c0a5cab32d216a9a09c23884e
Shusein714 <35206159+SHusein714@users.noreply.github.com>
2019-11-18 19:37:41 -0600

    Testing

--- src/edu/metrostate/ics372/project2/TestingHelloWorld.java
+++ src/edu/metrostate/ics372/project2/TestingHelloWorld.java
@@ -3,7 +3,7 @@ package edu.metrostate.ics372.project2;
 public class TestingHelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Testing github");
 
 	}
 
